79d8879b4640f7e1a7b8b811c358eee0fcec0efe
DreamMaoMao <2523610504@qq.com>
2024-04-17 07:59:39 +0800

    add hycov shotcut

--- main.cpp
+++ main.cpp
@@ -44,12 +44,14 @@ void addEasyMotionKeybinds()
 	// int key_idx;
 	g_pKeybindManager->addKeybind(SKeybind{"escape", 0, 0, 0, "easymotionexit", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
 
-	// test
-	// g_pKeybindManager->addKeybind(SKeybind{"q", 0, 0, g_pKeybindManager->stringToModMask("alt"), "killactive", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
-	// g_pKeybindManager->addKeybind(SKeybind{"left", 0, 0, g_pKeybindManager->stringToModMask("alt"), "killactive", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
-	// g_pKeybindManager->addKeybind(SKeybind{"right", 0, 0, g_pKeybindManager->stringToModMask("alt"), "killactive", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
-	// g_pKeybindManager->addKeybind(SKeybind{"up", 0, 0, g_pKeybindManager->stringToModMask("alt"), "killactive", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
-	// g_pKeybindManager->addKeybind(SKeybind{"down", 0, 0, g_pKeybindManager->stringToModMask("alt"), "killactive", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
+	// hycov shortcut
+	g_pKeybindManager->addKeybind(SKeybind{"tab", 0, 0, g_pKeybindManager->stringToModMask("alt"), "hycov:leaveoverview", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
+	g_pKeybindManager->addKeybind(SKeybind{"tab", 0, 0, g_pKeybindManager->stringToModMask("alt"), "easymotionexit", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
+	g_pKeybindManager->addKeybind(SKeybind{"q", 0, 0, g_pKeybindManager->stringToModMask("alt"), "killactive", "", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
+	g_pKeybindManager->addKeybind(SKeybind{"left", 0, 0, g_pKeybindManager->stringToModMask("alt"), "hycov:movefocus", "leftcross", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
+	g_pKeybindManager->addKeybind(SKeybind{"right", 0, 0, g_pKeybindManager->stringToModMask("alt"), "hycov:movefocus", "rightcross", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
+	g_pKeybindManager->addKeybind(SKeybind{"up", 0, 0, g_pKeybindManager->stringToModMask("alt"), "hycov:movefocus", "upcross", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
+	g_pKeybindManager->addKeybind(SKeybind{"down", 0, 0, g_pKeybindManager->stringToModMask("alt"), "hycov:movefocus", "downcross", 0, "__easymotionsubmap__", 0, 0, 0, 0, 0, 0});
 
 
 	//catchall
